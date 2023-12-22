986baa9f0176a6f3959c3cff941862c66d5276fd
Ogochukwu Ozotta <88560609+ogochukwu-ozotta@users.noreply.github.com>
2023-12-22 14:19:20 -0600

    add while loop for script to keep running

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import requests
 import selectorlib
 import smtplib
@@ -47,12 +48,14 @@ def read(extracted):
 
 
 if __name__ == "__main__":
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    print(extracted)
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
 
-    content = read(extracted)
-    if extracted != "No upcoming tours":
-        if extracted not in content:
-            store(extracted)
-            send_email(message="Hey, new event was found!")
+        content = read(extracted)
+        if extracted != "No upcoming tours":
+            if extracted not in content:
+                store(extracted)
+                send_email(message="Hey, new event was found!")
+        time.sleep(2)
